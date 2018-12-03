f3989111eee8726dd069d4d20725e4299b38d1ea
Joel <Joel@JOEL>
2018-12-03 18:03:49 +0100

    Simplified Path() function-usage

--- AdventOfCode/Services/FileManager.cs
+++ AdventOfCode/Services/FileManager.cs
@@ -12,8 +12,7 @@ namespace AdventOfCode
         List<T> ReadFromFileToList<T>(int day);
         string ReadLineFromFile(int day);
         string[] ReadFromFile(int day);
-        void SaveToFile(string fileName, string message, bool writeAllAtOnce);
-        string Path();
+        string Path(int day);
     }
 
     public class FileManager : IFileManager
@@ -25,7 +24,7 @@ namespace AdventOfCode
             
             List<T> data = new List<T>();
             const Int32 BufferSize = 128;
-            using (var fileStream = File.OpenRead(Path() + _translate.Day(day) + ".txt"))
+            using (var fileStream = File.OpenRead(Path(day)))
             using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
             {
                 String line;
@@ -40,7 +39,7 @@ namespace AdventOfCode
         public string ReadLineFromFile(int day)
         {
             const Int32 BufferSize = 128;
-            using (var fileStream = File.OpenRead(Path() + _translate.Day(day) + ".txt"))
+            using (var fileStream = File.OpenRead(Path(day)))
             using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
             {
                 String line;
@@ -55,38 +54,15 @@ namespace AdventOfCode
 
         public string[] ReadFromFile(int day)
         {
-            return File.ReadAllLines(Path() + _translate.Day(day) + ".txt");
+            return File.ReadAllLines(Path(day));
         }
 
-        public string Path()
+        public string Path(int day)
         {
             var directory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
-            return Directory.GetParent(directory).FullName + "/AdventOfCode/Resources/Files/";
+            return Directory.GetParent(directory).FullName + "/AdventOfCode/Resources/Files/"+_translate.Day(day)+".txt";
         }
 
-        public void SaveToFile(string fileName, string message, bool writeAllAtOnce)
-        {
-            var sb = new StringBuilder();
-            if (writeAllAtOnce)
-            {
-                sb.Append(message);
-            }
-
-            if (!File.Exists(Path() + fileName + ".txt") && !writeAllAtOnce)
-            {
-                using (StreamWriter file = new StreamWriter(Path() + fileName + ".txt"))
-                {
-                    if (writeAllAtOnce)
-                    {
-                        file.Write(sb.ToString()); //write a bunch
-                    }
-                    else
-                    {
-                        file.WriteLine(message); //write a line to a file
-                    }
-                }
-            }
-        }
     }
 
 
